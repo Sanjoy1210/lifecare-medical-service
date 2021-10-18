a102027ee0b4a3ff2fd0aaccd05f608d630bc2de
Sanjoy1210 <sanjoy.paul@hotmail.com>
2021-10-18 23:36:24 +0600

    footer ready

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      <h2>This is Footer</h2>
+    <div className="footer bg-secondary">
+      <div className="container my-5 text-start pt-5">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+          <div className="col">
+            <div className="logo">
+              <h1>LifeCare</h1>
+              <p>We make Quality HealthCare</p>
+            </div>
+          </div>
+          <div className="col">
+            <div className="title">
+              <h3>Useful Links</h3>
+              <hr />
+            </div>
+            <div className="links">
+              <ul>
+                <li>Home</li>
+                <li>Service</li>
+                <li>Blog</li>
+                <li>Contact</li>
+                <li>About us</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col">
+            <div className="title">
+              <h3>Newsletter</h3>
+              <hr />
+            </div>
+            <div className="newsletter-body">
+              <p>Sign up with your name and email to get updates fresh updates.</p>
+              <form>
+                <div className="mb-3">
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address" />
+                </div>
+                <div className="mb-3">
+                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+          <div className="col">
+            <div className="title">
+              <h3>Get in Touch</h3>
+              <hr />
+            </div>
+            <div className="contact-details">
+              <p>Podcasting operational change management inside of workflows to establish a framework.</p>
+              <p>Medical Ltd, Manhattan 1258,New York, USA Lahore</p>
+              <p>(+1) 234 567 8901</p>
+              <p>hello@website.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="copyright border-top py-4">
+        <div className="container copyright-content">
+          <div className="copyright-text">
+            <p className="mb-0">&copy; 2021 LifeCare Clinic. DEVELOPED BY Sanjoy Paul.</p>
+          </div>
+          <div className="social-icons">
+            <ul className="icons-list">
+              <li><i className="fab fa-facebook-f"></i></li>
+              <li><i className="fab fa-instagram"></i></li>
+              <li><i className="fab fa-twitter"></i></li>
+              <li><i className="fab fa-youtube"></i></li>
+              <li><i className="fab fa-linkedin"></i></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
