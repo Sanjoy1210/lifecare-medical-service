4e0597b561e64a323888936d1cbaedd667e6770b
Sanjoy1210 <sanjoy.paul@hotmail.com>
2021-10-20 19:31:12 +0600

    logo added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import './Header.css';
+import lifeCare from '../../../images/life-care.png';
 
 const Header = () => {
   const { user, logOut } = useAuth();
@@ -9,7 +10,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand" href="#">LifeCare</a>
+        <a className="navbar-brand" href="#"><img src={lifeCare} alt="" width="100px" /></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
